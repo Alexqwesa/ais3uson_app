5dc2f9aaaeac5dec41068ae5d634c6250800ae7f
Alex Savin <alexo.veto@gmail.com>
2021-12-06 00:11:34 +0300

    minor changes: global text

--- lib/src/global.dart
+++ lib/src/global.dart
@@ -1,5 +1,5 @@
 const String SERVER = "http://80.87.196.11";
 String qrData =
-    '''{"app": "AIS3USON web", "name": "test", "api_key": "123", "otd_id": 1, "otd": "otd test", "db": "kcson", "host": "192.168.0.102", "port": "48080"}''';
+    '''{"app": "AIS3USON web", "name": "test", "api_key": "123", "otd_id": 1, "otd": "Тестовое отделение", "db": "kcson", "host": "192.168.0.102", "port": "48080"}''';
 String qrData2 =
-    '''{"app": "AIS3USON web", "name": "test", "api_key": "123", "otd_id": 2, "otd": "otd222 test", "db": "kcson", "host": "192.168.0.102", "port": "48080"}''';
+    '''{"app": "AIS3USON web", "name": "test", "api_key": "123", "otd_id": 2, "otd": "Тестовое отделение 2", "db": "kcson", "host": "192.168.0.102", "port": "48080"}''';
