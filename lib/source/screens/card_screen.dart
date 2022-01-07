ec78203cedd9cb47dfe24c0f68a3c5ba531ea0ad
Savin Alexander Victorovich <alexo.veto@gmail.com>
2022-01-07 17:19:38 +0300

    card_screen: fix spacing and alignments

--- lib/source/screens/card_screen.dart
+++ lib/source/screens/card_screen.dart
@@ -21,7 +21,10 @@ class CardScreen extends StatefulWidget {
 class _CardScreenState extends State<CardScreen> {
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width < height
+        ? MediaQuery.of(context).size.width
+        : height;
 
     return Scaffold(
       appBar: AppBar(
@@ -29,135 +32,137 @@ class _CardScreenState extends State<CardScreen> {
           widget.service.shortText,
         ),
       ),
-      body: Card(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(12.0, 15, 12, 12),
-              child: SizedBox(
-                height: width / 3,
-                child: Row(
-                  children: [
-                    Spacer(),
-                    //
-                    // > service state icons
-                    //
-                    Expanded(
-                      flex: 1,
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: Transform.scale(
-                          scale: 2.5,
-                          child: ServiceCardState(
-                            clientService: widget.service,
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    Spacer(),
-                    //
-                    // > service image
-                    //
-                    Expanded(
-                      flex: 6,
-                      child: Center(
-                        child: SizedBox(
-                          height: width / 2,
-                          width: width / 2,
-                          child: Image.asset(
-                            'images/${widget.service.image}',
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    Spacer(),
-                    Expanded(
-                      flex: 3,
-                      // height: width / 2,
-                      // width: width / 4,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FittedBox(
-                            fit: BoxFit.fitHeight,
-                            child: IconButton(
-                              onPressed: () {
-                                widget.service.add();
-                              },
-                              icon: Transform.scale(
-                                scale: 2.5,
-                                child: const Icon(
-                                  Icons.publish_rounded,
-                                  color: Colors.green,
-                                ),
-                              ),
+      body: SizedBox(
+        width: width,
+        child: Card(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 15, 12, 12),
+                child: SizedBox(
+                  height: width / 3,
+                  child: Row(
+                    children: [
+                      const Spacer(),
+                      //
+                      // > service state icons
+                      //
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.topLeft,
+                          child: Transform.scale(
+                            scale: 2.5,
+                            child: ServiceCardState(
+                              clientService: widget.service,
                             ),
                           ),
-                          Spacer(),
-                          FittedBox(
-                            fit: BoxFit.fitHeight,
-                            child: IconButton(
-                              onPressed: () {
-                                widget.service.delete();
-                              },
-                              icon: Transform.rotate(
-                                angle: 3.14,
-                                child: Transform.scale(
+                        ),
+                      ),
+
+                      const Spacer(),
+                      //
+                      // > service image
+                      //
+                      Expanded(
+                        flex: 6,
+                        child: Center(
+                          child: SizedBox(
+                            height: width / 2,
+                            width: width / 2,
+                            child: Image.asset(
+                              'images/${widget.service.image}',
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      const Spacer(),
+                      Expanded(
+                        flex: 3,
+                        // height: width / 2,
+                        // width: width / 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            FittedBox(
+                              fit: BoxFit.fitHeight,
+                              child: IconButton(
+                                onPressed: () {
+                                  widget.service.add();
+                                },
+                                icon: Transform.scale(
                                   scale: 2.5,
                                   child: const Icon(
                                     Icons.publish_rounded,
-                                    color: Colors.red,
+                                    color: Colors.green,
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            //
-            // > service text
-            //
-            Center(
-              child: SizedBox(
-                // height: width * 1.2 - 102,
-                // width: 200,
-                child: SingleChildScrollView(
-                  physics: const NeverScrollableScrollPhysics(),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          widget.service.shortText,
-                          textScaleFactor: 1.1,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(16, 0, 16, 4),
-                        child: Text(
-                          widget.service.servTextAdd,
-                          softWrap: true,
-                          textAlign: TextAlign.justify,
-                          // overflow: TextOverflow.ellipsis,
+                            const Spacer(),
+                            FittedBox(
+                              fit: BoxFit.fitHeight,
+                              child: IconButton(
+                                onPressed: () {
+                                  widget.service.delete();
+                                },
+                                icon: Transform.rotate(
+                                  angle: 3.14,
+                                  child: Transform.scale(
+                                    scale: 2.5,
+                                    child: const Icon(
+                                      Icons.publish_rounded,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
                 ),
               ),
-            ),
-          ],
+              //
+              // > service text
+              //
+              Center(
+                child: SizedBox(
+                  // height: width * 1.2 - 102,
+                  // width: 200,
+                  child: SingleChildScrollView(
+                    physics: const NeverScrollableScrollPhysics(),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            widget.service.shortText,
+                            textScaleFactor: 1.1,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(16, 0, 16, 4),
+                          child: Text(
+                            widget.service.servTextAdd,
+                            softWrap: true,
+                            textAlign: TextAlign.justify,
+                            // overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
