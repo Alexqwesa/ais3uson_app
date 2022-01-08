5516dc7b3fa711014e48394a38852219182b574b
Savin Alexander Victorovich <alexo.veto@gmail.com>
2022-01-08 21:28:12 +0300

    minor lint changes, docs improves...

--- regenerate_documentation.sh
+++ regenerate_documentation.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
-projectDir=`dirname $0`
-cd $projectDir
+projectDir=`pwd`
+#cd $projectDir
+
+if [ `basename $projectDir`  !=  'ais3uson_app' ] ; then
+  echo 'Current dir is not ais3uson_app'
+  echo `basename $projectDir`
+  exit 1
+fi
+
+# Clean old
+rm -rf doc/api/
 
 # Generate
 dartdoc
 
 # Add missed files
-for f in `find ./lib/ -name '*.png' -print` ; do
+for f in $(find ./lib/ -name "*.png" -print) ; do
     dir=`dirname $f`
-    dir=${dir#'./lib/'}
+    dir=${dir#"./lib/"}
     dir=${dir//"/"/"_"}
-    cp -r $f doc/api/${dir}_`basename $f .png`/`basename $f`
+    cp -r "$f" "doc/api/${dir}_$(basename $f .png)/$(basename $f)"
 done
 
 cp LICENSE doc/api/
+mkdir doc/api/images/
+cp images/license.pdf doc/api/images/
+cp assets/ais-3uson-logo-128.png doc/api/assets/
 
 dhttpd --path doc/api &
 
-xdg-open  http://localhost:8080
+xdg-open  http://localhost:8080 &
