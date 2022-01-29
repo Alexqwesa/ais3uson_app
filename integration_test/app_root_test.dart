6c35955b4d77f0e835d269c87a2903adc8587cbf
Savin Alexander Victorovich <alexo.veto@gmail.com>
2022-01-29 22:41:57 +0300

    add first integration test

---
+++ integration_test/app_root_test.dart
@@ -0,0 +1,16 @@
+import 'package:ais3uson_app/app_root.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Main window', (tester) async {
+    await tester.pumpWidget(const AppRoot());
+    await tester.pumpAndSettle();
+    // Verify start screen
+    expect(find.text('Тестовое отделение 2'), findsNothing);
+    expect(find.text('Тестовое отделение'), findsNothing);
+    expect(find.text('Авторизируйтесь (отсканируйте QR код)'), findsOneWidget);
+  });
+}
