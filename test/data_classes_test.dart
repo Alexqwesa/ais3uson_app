9a9e07584527ff88ac085f7527df8bd4f4e4bf0f
Savin Alexander Viktorovich <alexo.veto@gmail.com>
2022-02-02 18:53:25 +0300

    new test: load serviceOfJournal from Hive

--- test/data_classes_test.dart
+++ test/data_classes_test.dart
@@ -4,7 +4,10 @@ import 'package:ais3uson_app/source/app_data.dart';
 import 'package:ais3uson_app/source/data_classes/worker_profile.dart';
 import 'package:ais3uson_app/source/from_json/worker_key.dart';
 import 'package:ais3uson_app/source/global_helpers.dart';
+import 'package:ais3uson_app/source/journal/service_of_journal.dart';
+import 'package:ais3uson_app/source/journal/service_state.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive_test/hive_test.dart';
 import 'package:http/http.dart' as http show Response;
 import 'package:mockito/mockito.dart';
@@ -23,7 +26,7 @@ void main() {
     //
     // > test create WorkerProfile
     //
-    test('create WorkerProfile', () async {
+    test('Create WorkerProfile', () async {
       expect(
         WorkerProfile(WorkerKey.fromJson(jsonDecode(qrData2))),
         isA<WorkerProfile>(),
@@ -32,7 +35,7 @@ void main() {
     //
     // > test json converters classes
     //
-    test('json converters classes', () async {
+    test('Json converters classes', () async {
       final wKey = WorkerKey.fromJson(jsonDecode(qrData2));
       expect(wKey, isA<WorkerKey>());
       expect(wKey.apiKey, (jsonDecode(qrData2) as Map)['api_key']);
@@ -41,7 +44,7 @@ void main() {
     //
     // > test addition of services
     //
-    test('test addition of service', () async {
+    test('Addition of service', () async {
       //
       // > prepare
       //
@@ -58,6 +61,7 @@ void main() {
       //
       // > start test
       //
+      expect(wp.clients[0].services[0].deleteAllowed, false);
       await wp.clients[0].services[0].add();
       expect(wp.clients[0].services[0].listDoneProgressError, [1, 0, 0]);
       // final body = '{"vdate":"2022-01-31","uuid":"${wp.journal.all.first.uid}",'
@@ -76,6 +80,46 @@ void main() {
         )).callCount,
         2,
       );
+      wp.dispose();
+    });
+    //
+    // > test addition of services
+    //
+    test('Load serviceOfJournal from Hive', () async {
+      //
+      // > prepare
+      //
+      final httpClient = AppData().httpClient;
+      final wp = WorkerProfile(WorkerKey.fromJson(jsonDecode(qrData2)));
+      final hive = await Hive.openBox<ServiceOfJournal>('journal_${wp.apiKey}');
+      await hive.add(
+        ServiceOfJournal(servId: 828, contractId: 1, workerId: 1),
+      );
+      final errorService =
+          ServiceOfJournal(servId: 828, contractId: 1, workerId: 1);
+      await hive.add(errorService);
+      errorService.state = ServiceState.rejected;
+      // delayed init, should look like values were loaded from hive
+      when(
+        httpClient.post(
+          Uri.parse('http://80.87.196.11:48080/add'),
+          headers: httpTestHeader,
+          body: anyNamed('body'),
+        ),
+      ).thenAnswer((_) async => http.Response('{"id": 1}', 200));
+      await wp.postInit();
+      //
+      // > start test
+      //
+      expect(wp.clients[0].services[0].listDoneProgressError, [0, 1, 1]);
+      expect(wp.clients[0].services[0].deleteAllowed, true);
+      await wp.clients[0].services[0].delete();
+      await wp.clients[0].services[0].add();
+      expect(wp.clients[0].services[0].listDoneProgressError, [1, 1, 0]);
+      await wp.clients[0].services[0].add();
+      expect(wp.clients[0].services[0].listDoneProgressError, [2, 1, 0]);
+      await wp.journal.commitAll();
+      expect(wp.clients[0].services[0].listDoneProgressError, [3, 0, 0]);
       wp.dispose();
     });
   });
