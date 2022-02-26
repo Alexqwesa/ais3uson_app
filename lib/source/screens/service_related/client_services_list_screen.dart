8eae3fbf77b4a7342601a7f8d8ed2ecae5c29f3a
Savin Alexander Victorovich <alexo.veto@gmail.com>
2022-02-26 18:58:34 +0300

    ClientServicesListScreen: use FutureBuilder

--- lib/source/screens/service_related/client_services_list_screen.dart
+++ lib/source/screens/service_related/client_services_list_screen.dart
@@ -1,7 +1,6 @@
 import 'package:ais3uson_app/source/app_data.dart';
 import 'package:ais3uson_app/source/data_classes/client_profile.dart';
 import 'package:ais3uson_app/source/data_classes/client_service.dart';
-import 'package:ais3uson_app/source/data_classes/worker_profile.dart';
 import 'package:ais3uson_app/source/screens/service_related/service_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -13,27 +12,16 @@ import 'package:provider/provider.dart';
 ///
 /// {@category UIServices}
 class ClientServicesListScreen extends StatefulWidget {
-  late final int profileNum;
-
-  late final int contractId;
-
-  late final ClientProfile client;
-
-  late final ClientProfile clientProfile;
-
-  late final WorkerProfile workerProfile;
+  late final Future<ClientProfile?> client;
 
   /// Show list of services assigned to client, allow input by click.
-  ClientServicesListScreen({Key? key}) : super(key: key) {
-    profileNum = AppData().lastScreen.profileNum;
-    contractId = AppData.instance.lastScreen.contractId;
-    client = AppData.instance.profiles[profileNum].clients
-        .firstWhere((element) => contractId == element.contractId);
-    workerProfile = AppData().profiles[profileNum];
-    clientProfile = AppData()
-        .profiles[profileNum]
-        .clients
-        .firstWhere((element) => element.contractId == contractId);
+  ClientServicesListScreen({
+    ClientProfile? clientProfile,
+    Key? key,
+  }) : super(key: key) {
+    client = clientProfile != null
+        ? Future(() async => clientProfile)
+        : AppData.instance.getLastClient();
   }
 
   @override
@@ -44,121 +32,145 @@ class ClientServicesListScreen extends StatefulWidget {
 class _ClientServicesListScreenState extends State<ClientServicesListScreen> {
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
-    return Scaffold(
-      //
-      // > appBar
-      //
-      appBar: AppBar(
-        title: ChangeNotifierProvider<ClientProfile>.value(
-          value: widget.clientProfile,
-          child: Row(
-            children: [
-              Expanded(
-                child: Consumer<ClientProfile>(
-                  builder: (context, data, child) {
-                    return Text(
-                      widget.client.name,
-                      overflow: TextOverflow.ellipsis,
-                    );
-                  },
-                ),
-              ),
-              IconButton(
-                icon: const Icon(Icons.refresh),
-                onPressed: () async {
-                  await widget.workerProfile.journal.archiveOldServices();
-                  await widget.workerProfile.journal.commitAll();
-                  await widget.workerProfile.syncHivePlanned();
-                },
-              ),
-            ],
-          ),
-        ),
-        actions: [
-          PopupMenuButton<dynamic>(
-            icon: const Icon(Icons.more_vert),
-            itemBuilder: (context) => <PopupMenuEntry>[
-              PopupMenuItem<ListTile>(
-                child: ListTile(
-                  leading: const Icon(Icons.grid_3x3),
-                  title: const Text('Подбробно'),
-                  onTap: () {
-                    Navigator.pop(context, '');
-                    setState(() {
-                      AppData.instance.serviceView = '';
-                    });
-                  },
-                ),
-              ),
-              PopupMenuItem<ListTile>(
-                child: ListTile(
-                  leading: const Icon(Icons.list_alt),
-                  title: const Text('Список'),
-                  onTap: () {
-                    Navigator.pop(context, 'tile');
-                    setState(() {
-                      AppData.instance.serviceView = 'tile';
-                    });
-                  },
-                ),
-              ),
-              PopupMenuItem<ListTile>(
-                child: ListTile(
-                  leading: const Icon(Icons.image),
-                  title: const Text('Значки'),
-                  onTap: () {
-                    Navigator.pop(context, 'square');
-                    setState(() {
-                      AppData.instance.serviceView = 'square';
-                    });
-                  },
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-      body: Center(
-        child: ChangeNotifierProvider<ClientProfile>.value(
-          value: widget.clientProfile,
-          child: SingleChildScrollView(
-            child: Consumer<ClientProfile>(
-              builder: (context, data, child) {
-                final servList = widget.clientProfile.services;
-                //
-                // > build list
-                //
-
-                return Container(
-                  child: servList.isNotEmpty
-                      ? Center(
-                          child: Wrap(
-                            children: servList.map(
-                              (element) {
-                                return ServiceCard(
-                                  service: element,
-                                  parentSize: size,
-                                );
-                              },
-                              // growable: false,
-                            ).toList(),
-                          ),
-                        )
-                      : Text(
-                          'Список положенных услуг пуст, \n\n'
-                          'возможно заведующий отделением уже закрыл договор\n\n'
-                          'обновите список',
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context).textTheme.headline5,
-                        ),
-                );
-              },
+    return FutureBuilder(
+      future: widget.client,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            appBar: AppBar(title: const Text('Загрузка')),
+            body: const Center(
+              child: CircularProgressIndicator(),
             ),
-          ),
-        ),
-      ),
+          );
+        } else if (snapshot.connectionState == ConnectionState.done &&
+            snapshot.data != null) {
+          final client = snapshot.data as ClientProfile;
+
+          final workerProfile = client.workerProfile;
+          final size = MediaQuery.of(context).size;
+
+          return Scaffold(
+            //
+            // > appBar
+            //
+            appBar: AppBar(
+              title: ChangeNotifierProvider<ClientProfile>.value(
+                value: client,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Consumer<ClientProfile>(
+                        builder: (context, data, child) {
+                          return Text(
+                            client.name,
+                            overflow: TextOverflow.ellipsis,
+                          );
+                        },
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.refresh),
+                      onPressed: () async {
+                        await workerProfile.journal.archiveOldServices();
+                        await workerProfile.journal.commitAll();
+                        await workerProfile.syncHivePlanned();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              actions: [
+                PopupMenuButton<dynamic>(
+                  icon: const Icon(Icons.more_vert),
+                  itemBuilder: (context) => <PopupMenuEntry>[
+                    PopupMenuItem<ListTile>(
+                      child: ListTile(
+                        leading: const Icon(Icons.grid_3x3),
+                        title: const Text('Подбробно'),
+                        onTap: () {
+                          Navigator.pop(context, '');
+                          setState(() {
+                            AppData.instance.serviceView = '';
+                          });
+                        },
+                      ),
+                    ),
+                    PopupMenuItem<ListTile>(
+                      child: ListTile(
+                        leading: const Icon(Icons.list_alt),
+                        title: const Text('Список'),
+                        onTap: () {
+                          Navigator.pop(context, 'tile');
+                          setState(() {
+                            AppData.instance.serviceView = 'tile';
+                          });
+                        },
+                      ),
+                    ),
+                    PopupMenuItem<ListTile>(
+                      child: ListTile(
+                        leading: const Icon(Icons.image),
+                        title: const Text('Значки'),
+                        onTap: () {
+                          Navigator.pop(context, 'square');
+                          setState(() {
+                            AppData.instance.serviceView = 'square';
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            body: Center(
+              child: ChangeNotifierProvider<ClientProfile>.value(
+                value: client,
+                child: SingleChildScrollView(
+                  child: Consumer<ClientProfile>(
+                    builder: (context, data, child) {
+                      final servList = client.services;
+
+                      //
+                      // > build list
+                      //
+                      return Container(
+                        child: servList.isNotEmpty
+                            ? Center(
+                                child: Wrap(
+                                  children: servList.map(
+                                    (element) {
+                                      return ServiceCard(
+                                        service: element,
+                                        parentSize: size,
+                                      );
+                                    },
+                                    // growable: false,
+                                  ).toList(),
+                                ),
+                              )
+                            : Text(
+                                'Список положенных услуг пуст, \n\n'
+                                'возможно заведующий отделением уже закрыл договор\n\n'
+                                'обновите список',
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context).textTheme.headline5,
+                              ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+          );
+        } else {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Ошибка'),
+            ),
+          );
+        }
+      },
     );
   }
 }
