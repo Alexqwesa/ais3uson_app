c98ff939ee0632662ce68d9a4b35a1d300023ae4
Savin Alexander Viktorovich <alexo.veto@gmail.com>
2022-03-09 14:45:00 +0300

    fix test (use localizedMaterialApp)

--- test/helpers/setup_and_teardown_helpers.dart
+++ test/helpers/setup_and_teardown_helpers.dart
@@ -1,4 +1,7 @@
+import 'package:ais3uson_app/generated/l10n.dart';
 import 'package:ais3uson_app/source/app_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'mock_server.dart';
@@ -15,4 +18,21 @@ Future<void> mySetUpAll() async {
   // > httpClient setup
   //
   AppData().httpClient = getMockHttpClient();
+}
+
+// ignore: avoid-returning-widgets
+MaterialApp localizedMaterialApp(Widget widget) {
+  return MaterialApp(
+    //
+    // > l10n
+    //
+    localizationsDelegates: const [
+      S.delegate,
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+    ],
+    supportedLocales: S.delegate.supportedLocales,
+    home: widget,
+  );
 }
