5b3273e10c4d7811dcfa1c20ae4e7d94f3dace7d
Savin Alexander Victorovich <alexo.veto@gmail.com>
2022-04-10 19:15:17 +0300

    docs: Base providers

--- lib/source/providers/providers.dart
+++ lib/source/providers/providers.dart
@@ -1,28 +1,25 @@
-// ignore_for_file: sort_constructors_first
-
 import 'package:ais3uson_app/source/journal/service_of_journal.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// Base provider of [SharedPreferences].
 final preference = FutureProvider<SharedPreferences>((ref) async {
   return SharedPreferences.getInstance();
 });
 
+/// Base provider of hive [Box], type [dynamic].
 final hiveBox = FutureProvider.family<Box<dynamic>, String>((ref, boxName) {
   return Hive.openBox<dynamic>(boxName);
 });
 
-final hiveStringBox =
-    FutureProvider.family<Box<String>, String>((ref, boxName) {
-  return Hive.openBox<String>(boxName);
-});
-
+/// Base provider of hive [Box], type [ServiceOfJournal].
 final hiveJournalBox =
     FutureProvider.family<Box<ServiceOfJournal>, String>((ref, boxName) {
   return Hive.openBox<ServiceOfJournal>(boxName);
 });
 
+/// Base provider of hive [Box], type [DateTime].
 final hiveDateTimeBox = FutureProvider.family<Box<DateTime>, String>(
   (ref, boxName) async => Hive.openBox<DateTime>(boxName),
 );
