a1c4029495bec821e74529e12d5e31db7ca5ec9d
Savin Alexander Viktorovich <alexo.veto@gmail.com>
2022-06-08 12:04:17 +0300

    new version of qr_code_scanner (back to main repo)

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -8,14 +8,11 @@
 
 import 'package:audioplayers_web/audioplayers_web.dart';
 import 'package:camera_web/camera_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:record_web/record_web.dart';
 import 'package:share_plus_web/share_plus_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
-import 'package:url_launcher_web/url_launcher_web.dart';
-import 'package:video_player_web/video_player_web.dart';
-import 'package:wakelock_web/wakelock_web.dart';
-
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:url_launcher_web/url_launcher_webdart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
@@ -25,7 +22,5 @@ void registerPlugins(Registrar registrar) {
   SharePlusPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
-  VideoPlayerPlugin.registerWith(registrar);
-  WakelockWeb.registerWith(registrar);
   registrar.registerMessageHandler();
 }
