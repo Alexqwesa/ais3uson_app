c437cafd3595be2e12a2423f378117040b64d926
Aleksandr Savin <alexo.veto@gmail.com>
2023-04-12 21:56:07 +0700

    minor rename: repositories

--- lib/repositories.dart
+++ lib/repositories.dart
@@ -2,5 +2,5 @@
 ///
 library repositories;
 
-export 'package:ais3uson_app/src/data_managers/journal_hive_repository.dart';
-export 'package:ais3uson_app/src/data_managers/journal_http_repository.dart';
+export 'package:ais3uson_app/src/repositories/journal_hive_repository.dart';
+export 'package:ais3uson_app/src/repositories/journal_http_repository.dart';
